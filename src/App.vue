0f114e6ccf699a1727dc0f9230124ca965384700
yasaswini-vrm <yashaswini.veluvolu@gmail.com>
2026-02-19 13:18:50 -0600

    ui base code

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,27 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+<div class="app">
+  <h1> Missing Me kit </h1>
+  <p> Soft Cozy Slightly Annoying </p>
+</div>
 </template>
 
+<script setup>
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.app {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: #f8f6ff;
+  font-family: system-ui, -apple-system, sans-serif;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+h1 {
+  font-size: 2 rem;
+  margin-bottom: 0.5 rem;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+p {
+  color: #555;
 }
 </style>
